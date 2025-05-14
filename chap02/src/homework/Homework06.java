4e041581cb1cb0ab37f41b5c94d5ffa0ffae2cb3
jeong gil hun <wjdrlfgns1@naver.com>
2025-05-14 17:20:47 +0900

    자바 과제

--- chap02/src/homework/Homework06.java
+++ chap02/src/homework/Homework06.java
@@ -6,6 +6,7 @@ public class Homework06 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		char ch = scanner.nextLine().charAt(0);
-		System.out.println(ch);
+		int num =Integer.parseInt(ch+"");
+		System.out.println(num*num);
 	}
 }
