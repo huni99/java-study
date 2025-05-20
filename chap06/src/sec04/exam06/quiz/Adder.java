d4648c7d24dd4abbcddef2ac0e95e84314929457
jeong gil hun <wjdrlfgns1@naver.com>
2025-05-20 15:48:23 +0900

    메소드 오버로딩

---
+++ chap06/src/sec04/exam06/quiz/Adder.java
@@ -0,0 +1,11 @@
+package sec04.exam06.quiz;
+
+public class Adder {
+	void add(int a, int b) {
+		System.out.println("합:"+ (a+b));
+	}
+	void add(double a, double b) {
+		System.out.println("합:"+(a+b));
+	}
+
+}
